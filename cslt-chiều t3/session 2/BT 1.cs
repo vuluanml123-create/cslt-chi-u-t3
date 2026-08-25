0f745db4751a72f573665515259021efe8f7ebe9
tranvuluan <vuluanml123@gmail.com>
2026-08-25 23:52:19 +0700

    BT ở lớp

---
+++ cslt-chiều t3/session 2/BT 1.cs
@@ -0,0 +1,59 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+namespace cslt_chiều_t3.session_3
+{
+    internal class Bt
+    {
+        public static void Main(string[] args)
+        {
+            double celsius = Convert.ToSByte(Console.ReadLine());
+            double kelvin = celsius + 273;
+            double fahrenheit = celsius * 18 / 10 + 32;
+            Console.WriteLine($"kelvin= {kelvin}");
+            Console.WriteLine($"fahrenheit= {fahrenheit}");
+        }*/
+
+/*using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Write("Nhập bán kính hình cầu: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double surface = 4 * Math.PI * Math.Pow(radius, 2);
+        double volume = (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+
+        Console.WriteLine($"Diện tích mặt cầu (Surface): {surface:F2}");
+        Console.WriteLine($"Thể tích khối cầu (Volume): {volume:F1}");
+    }
+}*/
+/*using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.Write("Nhập số thứ nhất: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nhập số thứ hai: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n--- Kết quả phép tính ---");
+        Console.WriteLine($"Phép cộng: {a} + {b} = {a + b}");
+        Console.WriteLine($"Phép trừ:  {a} - {b} = {a - b}");
+        Console.WriteLine($"Phép nhân: {a} x {b} = {a * b}");
+        Console.WriteLine($"Phép chia: {a} / {b} = {a / b}");
+        Console.WriteLine($"Chia lấy dư (mod): {a} mod {b} = {a % b}");
+    }
+}*/
---
+++ cslt-chiều t3/session 2/BTVN 1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cslt_chiều_t3.session_2
+{
+    internal class BTVN
+    {
+    }
+}
